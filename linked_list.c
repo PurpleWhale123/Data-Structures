290b27a372f0085fdb5331a7574facc27f07d1be
PurpleWhale <59802469+PurpleWhale123@users.noreply.github.com>
2022-04-05 18:47:24 +0300

    Remove node functions
    Implemented 3 functions:
    remove_node_at_the_start
    remove_node_at_the_end
    remove_node_at_index

--- linked_list.c
+++ linked_list.c
@@ -8,11 +8,16 @@ struct Node {
 	node_t* next;
 };
 
+
 node_t* create_node(int);
 
 void insert_node_at_the_start(node_t**, node_t*);
 void insert_node_at_the_end(node_t*, node_t*);
 void insert_node_at_index(node_t*, node_t*, int);
+
+void remove_node_at_the_start(node_t**);
+void remove_node_at_the_end(node_t*);
+void remove_node_at_index(node_t*, int);
 
 int get_length_of(node_t*_list);
 
@@ -20,8 +25,9 @@ void print_list(node_t*);
 
 int main() {
 	node_t* list = create_node(10);
+	insert_node_at_the_end(list, create_node(35));
 	insert_node_at_the_end(list, create_node(15));
-	insert_node_at_the_start(&list, create_node(32));
+	remove_node_at_index(list, 1);
 	print_list(list);
 	
 	printf("Length: %d", get_length_of(list));
@@ -50,7 +56,8 @@ void insert_node_at_index(node_t* list, node_t* node, int index) {
 	
 	if(index == 0) {
 		printf("You can use insert_node_at_the_start() instead");
-		insert_node_at_the_start(list, node);
+		insert_node_at_the_start(&list, node);
+		return;
 	}
 	
 	if(index > length || index < 0) {
@@ -67,6 +74,49 @@ void insert_node_at_index(node_t* list, node_t* node, int index) {
 void insert_node_at_the_start(node_t** list, node_t* node) {
 	node->next = *list;
 	*list = node;
+}
+
+void remove_node_at_the_start(node_t** list) {
+	node_t* temp = *list;
+	free(temp);
+	*list = (*list)->next;
+}
+
+void remove_node_at_index(node_t* list, int index) {
+	if(list == NULL) return;
+	
+	int length = get_length_of(list);
+	
+	if(index == 0) {
+		printf("You can use remove_node_at_index() instead");
+		remove_node_at_the_start(&list);\
+		return;
+	}
+	
+	if(index > length || index < 0) {
+		printf("Out of bounds :(\nYou can't use index that is greater than length or less than zero\n'");
+		return;
+	}
+	
+	node_t* current = list;
+	for(int i = 0; i < index - 1; i++) current = current->next;
+	if(current->next->next) {
+		node_t* temp = current->next;
+		current->next = current->next->next;
+		free(temp);
+	} else {
+		free(current->next);
+		current->next = NULL;
+	}
+}
+
+void remove_node_at_the_end(node_t* list) {
+	if(list == NULL) return;
+	
+	node_t* current = list;
+	while(current->next->next) current = current->next;
+	free(current->next);
+	current->next = NULL;
 }
 
 void insert_node_at_the_end(node_t* list, node_t* node) {
